7717202144d963bbd94319689455a84b45d8c1f7
prestoine <Preston@desouzafernandez.com>
2024-08-10 19:41:35 +0000

    deploy and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yourusername.github.io',
+  base: '/your-repo-name',
+  integrations: [mdx(), sitemap()],
+});
