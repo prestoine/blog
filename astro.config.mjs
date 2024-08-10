b31f3d9a24252301a25a46badd6e6ef0755838e9
prestoine <Preston@desouzafernandez.com>
2024-08-10 19:51:47 +0000

    change deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/your-repo-name',
-  integrations: [mdx(), sitemap()],
+  site: 'https://prestoine.github.io',
+  base: '/blog',
 });
